abfe9257d92de7cc02b5b1ac93a2436cbe52d484
Ambika Kumari <ambikaisone@gmail.com>
2025-09-19 02:42:57 +0530

    Made the dashboard

--- src/pages/components/featureCard.js
+++ src/pages/components/featureCard.js
@@ -1,11 +1,23 @@
+// src/components/Card.js
+import Image from "next/image";
 
-
-const featureCard = () => {
+const FeatureCard = ({ logo, title, description }) => {
   return (
-    <div>
-      
+    <div className="w-64 h-[200px] bg-white rounded-md m-8 flex flex-col items-center p-4 text-center shadow-md hover:shadow-lg transition-shadow">
+      {/* Logo */}
+      <Image
+        src={logo}
+        alt={title}
+        width={64}
+        height={64}
+        className="h-1/2 object-contain mb-2"
+      />
+      {/* Title */}
+      <h3 className="text-lg font-bold">{title}</h3>
+      {/* Description */}
+      <p className="text-gray-600 text-sm mt-1">{description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default feature-card;
+export default FeatureCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        greenfooter:"#3F9148",
+        background:"#FCF9F6",
+        
+      }
+    },
   },
   plugins: [],
 };
