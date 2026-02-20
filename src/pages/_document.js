93cad4b1bf74ba0ce0e90768c73e70644557efde
MeSanskarSingh <sanskar.singh.168@gmail.com>
2026-02-20 05:52:45 +0530

    Added favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <body className="antialiased">
         <Main />
